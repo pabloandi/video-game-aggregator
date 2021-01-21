e838dc679c39598395533a74fec14f8517803b2b
Pablo Diaz <pabloandi@gmail.com>
2021-01-21 18:53:46 -0500

    init commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laracasts Video Games</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/images/laracasts-logo.svg" alt="laracasts games" class="w-32 flex-none">
+                </a>
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" name="" id="" class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outline w-64 px-3 pl-8 py-1" placeholder="Search...">
+                    <div class="absolute top-0 flex items-center h-full ml-2">
+                        <svg class="fill-current text-gray-400 w-4" viewBox="0 0 512 512">
+                            <title>ionicons-v5-f</title>
+                            <path d="M456.69,421.39,362.6,327.3a173.81,173.81,0,0,0,34.84-104.58C397.44,126.38,319.06,48,222.72,48S48,126.38,48,222.72s78.38,174.72,174.72,174.72A173.81,173.81,0,0,0,327.3,362.6l94.09,94.09a25,25,0,0,0,35.3-35.3ZM97.92,222.72a124.8,124.8,0,1,1,124.8,124.8A124.95,124.95,0,0,1,97.92,222.72Z"/>
+                          </svg>
+                    </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="/images/avatar.jpg" alt="avatar" class="rounded-full w-8"></a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
